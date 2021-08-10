cafc88638433ca220c76fcbc4a4e06b93a5835be
Stanislav Zhatikov <stanislav.zhatikov@dsr-corporation.com>
2021-08-10 15:21:08 +0300

    search component

--- site/src/components/Search/Search.tsx
+++ site/src/components/Search/Search.tsx
@@ -1,12 +1,16 @@
 import * as React from 'react'
-import { FormEventHandler, useState } from 'react'
+import { useState } from 'react'
+import { Icons, SvgIcon } from '../SvgIcon/SvgIcon'
+import * as styles from './Search.module.scss'
+import { cls } from '../../common/utils'
 
 type Props = {
   onSearch: (query: string) => void
   query?: string
+  bordered?: boolean
 }
 
-export const Search = ({onSearch, query = ''}: Props) => {
+export const Search = ({ onSearch, query = '', bordered }: Props) => {
   const [value, setValue] = useState(query)
   const onSubmit = (e) => {
     e.preventDefault()
@@ -14,17 +18,40 @@ export const Search = ({onSearch, query = ''}: Props) => {
   }
 
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <div>
-          <label>
-            <span>Search</span>
-            <input value={value} onChange={e => setValue(e.target.value)} autoCorrect='off' placeholder='Search protocols to use in your own software' autoComplete='off'
-                   type='search' aria-label='Search protocols definitions' />
-          </label>
-        </div>
-        <button type='submit'>Search</button>
-      </form>
-    </div>
+    <form className={styles.search} onSubmit={onSubmit} role="search">
+      <label className="visually-hidden" htmlFor="search">
+        Search protocols definitions
+      </label>
+      <input
+        className={cls(styles.input, 'rounded', bordered && styles.bordered, 'hide-mobile')}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        autoCorrect="off"
+        placeholder="Search protocols to use in your own software"
+        autoComplete="off"
+        type="text"
+        aria-required="true"
+        required
+        id="search"
+      />
+      <input
+        className={cls(styles.input, 'rounded', bordered && styles.bordered, 'hide-desktop')}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        autoCorrect="off"
+        placeholder="Search protocols"
+        autoComplete="off"
+        type="text"
+        aria-required="true"
+        required
+        id="search"
+      />
+      <button className={styles.clearButton} onClick={(_) => setValue('')} aria-label="Clear search text" type="button">
+        <SvgIcon icon={Icons.cross} className={styles.iconCross} />
+      </button>
+      <button className={styles.button} type="submit" aria-label="Search for protocols">
+        <SvgIcon icon={Icons.magnify} className={styles.iconMagnify} />
+      </button>
+    </form>
   )
 }
