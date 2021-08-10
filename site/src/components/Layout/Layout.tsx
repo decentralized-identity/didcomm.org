bb0417f0b9ab43142adbe39f03066232ac8ab270
Stanislav Zhatikov <stanislav.zhatikov@dsr-corporation.com>
2021-08-10 15:24:23 +0300

    layout component

--- site/src/components/Layout/Layout.tsx
+++ site/src/components/Layout/Layout.tsx
@@ -1,17 +1,20 @@
 import * as React from 'react'
 import { PropsWithChildren } from 'react'
 import { Seo } from '../Seo/Seo'
+import * as styles from './Layout.module.scss'
+import { cls } from '../../common/utils'
 
 type Props = PropsWithChildren<{
   title?: string
+  primary?: boolean
+  accent?: boolean
 }>
 
-
-export const Layout = ({ title, children }: Props) => {
+export const Layout = ({ title, primary, accent, children }: Props) => {
   return (
     <>
       <Seo title={title} />
-      <div>{children}</div>
+      <div className={cls(styles.layout, primary && styles.primary, accent && styles.accent)}>{children}</div>
     </>
   )
 }
