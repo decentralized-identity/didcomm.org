1af8df5f158f41ecc560222a7bb157c79c264d4b
Stanislav Zhatikov <stanislav.zhatikov@dsr-corporation.com>
2021-08-10 15:24:37 +0300

    pagination component

--- site/src/components/Pagination/Pagination.tsx
+++ site/src/components/Pagination/Pagination.tsx
@@ -1,25 +1,43 @@
 import * as React from 'react'
 
+import * as styles from './Pagination.module.scss'
+import { cls } from '../../common/utils'
+
 type Props = {
   pageCount: number
+  currentPage: number
   setPage: (page: number) => void
+  next: () => void
+  hasNext: boolean
 }
 
-export const Pagination = ({ setPage, pageCount }: Props) => (
-  pageCount > 1
-    ? (
-      <nav>
-        <ul>
-          {getPages(pageCount).map(page => (
-            <li key={page}>
-              <button onClick={() => setPage(page)}>{page}</button>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    )
-    : null
-)
+export const Pagination = ({ setPage, pageCount, currentPage, next, hasNext }: Props) => {
+  const pages = getPages(pageCount, currentPage)
+  return pageCount > 1 ? (
+    <nav className={styles.pagination}>
+      {hasNext && (
+        <button onClick={next} className={cls('font-callout', styles.next)}>
+          Next
+        </button>
+      )}
+      <ul className={styles.pages}>
+        {pages.map((page) => (
+          <li key={page}>
+            <button className={cls('font-callout', styles.page, page === currentPage && styles.current)} onClick={() => setPage(page)}>
+              {page}
+            </button>
+          </li>
+        ))}
+      </ul>
+      {pages[pages.length - 1] < pageCount && <div className={styles.ellipsis}>...</div>}
+    </nav>
+  ) : null
+}
 
-
-const getPages = (pageCount: number) => (Array.from({ length: pageCount }, (_, i) => i + 1))
+const getPages = (pageCount: number, curPage: number) => {
+  let startPosition = Math.floor((curPage - 1) / 5) * 5
+  if (pageCount - curPage < 5 && curPage > 5) {
+    startPosition -= 5 - (pageCount - startPosition)
+  }
+  return Array.from({ length: Math.min(pageCount, 5) }, (_, i) => i + 1 + startPosition)
+}
