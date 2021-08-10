09a19918efdb663f5a653764eec1faa0e7f49960
Stanislav Zhatikov <stanislav.zhatikov@dsr-corporation.com>
2021-08-10 15:25:12 +0300

    protocols list component

--- site/src/components/ProtocolsList/ProtocolsList.tsx
+++ site/src/components/ProtocolsList/ProtocolsList.tsx
@@ -1,24 +1,26 @@
-import { Link } from 'gatsby'
 import * as React from 'react'
 import { usePagination } from '../../common/hooks/usePagination'
 import { Pagination } from '../Pagination/Pagination'
 import { Protocol } from '../../common/types'
+import { ProtocolsListItem } from './ProtocolsListItem/ProtocolsListItem'
+import * as styles from './ProtocolsList.module.scss'
 
 type Props = {
   protocols: Protocol[]
 }
 
 export const ProtocolsList = ({ protocols }: Props) => {
-  const {items: paginatedProtocols, setPage, pagesCount} = usePagination(protocols)
-
+  const { items: paginatedProtocols, setPage, pagesCount, page, next, hasNext } = usePagination(protocols)
   return (
     <>
-      <ul>
-        {paginatedProtocols.map(protocol => (
-          <li key={protocol.slug}><Link to={`/${protocol.slug}`}>{protocol.title}</Link></li>
+      <ul className={styles.list}>
+        {paginatedProtocols.map((protocol) => (
+          <li key={protocol.slug}>
+            <ProtocolsListItem protocol={protocol} />
+          </li>
         ))}
       </ul>
-      <Pagination pageCount={pagesCount} setPage={setPage} />
+      <Pagination hasNext={hasNext} next={next} currentPage={page} pageCount={pagesCount} setPage={setPage} />
     </>
   )
 }
